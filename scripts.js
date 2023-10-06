4537f17ee168ef0121c8f24a4df9a309b2496d97
Santiago Cuevas <santiagocuevas77@gmail.com>
2023-10-05 19:23:13 -0600

    DOM Manipulation.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
+const zeroBtn = document.querySelector(".zero");
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const fiveBtn = document.querySelector(".five");
+const sixBtn = document.querySelector(".six");
+const sevenBtn = document.querySelector(".seven");
+const eightBtn = document.querySelector(".eight");
+const nineBtn = document.querySelector(".nine");
+
+const clearBtn = document.querySelector("clear");
+const equalsBtn = document.querySelector(".equals");
+const addBtn = document.querySelector(".add");
+const subractBtn = document.querySelector(".subtract");
+const multiplyBtn = document.querySelector(".multiply");
+const divideBtn = document.querySelector(".divide");
+
+const calcDisplay = document.getElementById("#display");
+
+let firstNum = undefined;
+let operator = undefined;
+let secondNum = undefined;
+
+function operate() {
+  return operator(firstNum, secondNum);
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -13,9 +41,3 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
-
-let firstNum = "";
-let operator = "";
-let secondNum = "";
-
-// const operate = operator(firstNum, secondNum);
