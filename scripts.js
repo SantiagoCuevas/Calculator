cf66ceacaf11d42ff7d4bfb8b2130b8eb405bebc
Santiago Cuevas <santiagocuevas77@gmail.com>
2023-10-11 19:57:41 -0600

    Implement operate function.

--- scripts.js
+++ scripts.js
@@ -1,43 +1,66 @@
 const numBtns = document.querySelectorAll(".num");
 const opBtns = document.querySelectorAll(".op");
-
+const equalBtn = document.querySelector(".equals");
+const clearBtn = document.querySelector(".clear");
 const calcDisplay = document.querySelector(".bottom");
 
 let firstNum = "";
-let operator = "";
 let secondNum = "";
+let operator = null;
+let answer = "";
 
-function operate() {
-  return operator(firstNum, secondNum);
-}
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+const evaluate = {
+  "÷": function (a, b) {
+    return a / b;
+  },
+  "×": function (a, b) {
+    return a * b;
+  },
+  "−": function (a, b) {
+    return a - b;
+  },
+  "+": function (a, b) {
+    return +a + +b;
+  },
+};
 
 numBtns.forEach((button) =>
   button.addEventListener("click", () => appendNum(button.textContent))
 );
 
-function appendNum(number) {
-  if (calcDisplay.textContent === "0") {
-    resetScreen();
-  }
-  calcDisplay.textContent += number;
+opBtns.forEach((operator) =>
+  operator.addEventListener("click", () => appendOp(operator.textContent))
+);
+
+clearBtn.addEventListener("click", resetScreen);
+
+equalBtn.addEventListener("click", operate);
+
+function operate() {
+  answer = evaluate[operator](firstNum, secondNum);
+  calcDisplay.textContent = answer;
+  firstNum = answer;
+  secondNum = "";
+  operator = null;
+}
+
+function appendNum(num) {
+  operator === null ? (firstNum += num) : (secondNum += num);
+  calcDisplay.textContent += num;
+}
+
+function appendOp(op) {
+  operator = op;
+  calcDisplay.textContent += " " + operator + " ";
 }
 
 function resetScreen() {
   calcDisplay.textContent = "";
+  resetValues();
+}
+
+function resetValues() {
+  firstNum = "";
+  secondNum = "";
+  operator = null;
 }
